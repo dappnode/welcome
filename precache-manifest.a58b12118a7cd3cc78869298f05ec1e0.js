ddacf0e31d7cec2ad72ec8e28767a373365ac5f8
dapplion <guillem.lion.benosa@gmail.com>
2019-05-06 17:01:27 +0200

    Updates

---
+++ precache-manifest.a58b12118a7cd3cc78869298f05ec1e0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d7efaaaeb3720d323ed4e74063db286",
+    "url": "/welcome.dappnode.io/index.html"
+  },
+  {
+    "revision": "99b6afda05ae69a94506",
+    "url": "/welcome.dappnode.io/static/css/main.1e6925a3.chunk.css"
+  },
+  {
+    "revision": "c200ae296c9719a358ac",
+    "url": "/welcome.dappnode.io/static/js/2.4ce78637.chunk.js"
+  },
+  {
+    "revision": "99b6afda05ae69a94506",
+    "url": "/welcome.dappnode.io/static/js/main.dcd36420.chunk.js"
+  },
+  {
+    "revision": "cfbcb466f9ffe4b185c9",
+    "url": "/welcome.dappnode.io/static/js/runtime~main.98d6a959.js"
+  },
+  {
+    "revision": "d5fb455d6e2e10c3b01c2ed5e902e46d",
+    "url": "/welcome.dappnode.io/static/media/connect-to-router.d5fb455d.png"
+  },
+  {
+    "revision": "1fe4e5d7e5ca24842f2027c6db32f0f6",
+    "url": "/welcome.dappnode.io/static/media/copyIcon.1fe4e5d7.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/welcome.dappnode.io/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d299d5406ba9e0aee94972054d044cb0",
+    "url": "/welcome.dappnode.io/static/media/ready-icon.d299d540.png"
+  },
+  {
+    "revision": "e4cba78291717ebbdaf1eed779eb2d16",
+    "url": "/welcome.dappnode.io/static/media/secure-file.e4cba782.svg"
+  }
+]);
