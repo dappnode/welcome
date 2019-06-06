d7017c42a8d911132f388ecd0343911c9184c28a
dapplion <guillem.lion.benosa@gmail.com>
2019-06-06 23:59:38 +0200

    Updates

---
+++ precache-manifest.e6dda3af01904ccbcb570645c1f04cab.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb75f4d6bd30f87827978fa1e8ff3de1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94e4fdaa3828520b8505",
+    "url": "/static/css/main.5337e80f.chunk.css"
+  },
+  {
+    "revision": "3ac92f7f911a6abf5e29",
+    "url": "/static/js/2.cff806e2.chunk.js"
+  },
+  {
+    "revision": "94e4fdaa3828520b8505",
+    "url": "/static/js/main.4416913c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d5fb455d6e2e10c3b01c2ed5e902e46d",
+    "url": "/static/media/connect-to-router.d5fb455d.png"
+  },
+  {
+    "revision": "1fe4e5d7e5ca24842f2027c6db32f0f6",
+    "url": "/static/media/copyIcon.1fe4e5d7.svg"
+  },
+  {
+    "revision": "d299d5406ba9e0aee94972054d044cb0",
+    "url": "/static/media/ready-icon.d299d540.png"
+  },
+  {
+    "revision": "e4cba78291717ebbdaf1eed779eb2d16",
+    "url": "/static/media/secure-file.e4cba782.svg"
+  }
+]);
